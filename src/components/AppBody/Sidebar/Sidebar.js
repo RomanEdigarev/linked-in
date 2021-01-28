646193a3e2508b4243a9d9450257ef8115a3c3e7
RomanEdigarev <romzes2_09@mail.ru>
2021-01-28 21:24:53 +0300

    stylilng sidebar

--- src/components/AppBody/Sidebar/Sidebar.js
+++ src/components/AppBody/Sidebar/Sidebar.js
@@ -3,14 +3,25 @@ import "./Sidebar.css";
 import Avatar from "@material-ui/core/Avatar";
 
 export const Sidebar = () => {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img
+          src="https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
         <Avatar className="sidebar__avatar" />
         <h2>Roman Edigarev</h2>
         <h4>romanedigariev@yandex.ru</h4>
       </div>
+
       <div className="sidebar__stats">
         <div className="sidebar__stat">
           <p>Who viewed you</p>
@@ -21,8 +32,14 @@ export const Sidebar = () => {
           <p className="sidebar__statNumber">2,351</p>
         </div>
       </div>
-      <div className="sidebar__buttom">
+
+      <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("developer")}
+        {recentItem("design")}
+        {recentItem("spb")}
       </div>
     </div>
   );
